3819d4841974707008bb85f66022cfb483ea3587
Jan Kankovsky <52216257+kankovskyj@users.noreply.github.com>
2024-05-11 00:09:36 +0200

    cross

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,14 +6,23 @@ type Props = {}
 
 const Hero = (props: Props) => {
   return (
-    <div className='lg:pt-20 pb-6 pt-20 bg-[#A2EB99] flex flex-col'>
-        <h1 className='text-3xl lg:text-5xl font-bold text-center tracking-tight text-[#26274b]'>Potřebujete půjčku, ale v bance vám ji nedají<span className='pl-1'>?</span></h1>
-        <p className='text-xl text-center lg:w-2/3 mx-auto pt-6 pb-6 text-[#26274b] leading-normal'>My vám půjčku schválíme do <UnderlineText strokeColor='#26274b' className='font-bold text-2xl pb-0.5 lg:pb-1'>72 hodin</UnderlineText>. Stačí vyplnit krátký dotazník a my se s vámi spojíme.</p>
-        <div className='relative mx-auto'>
-       
-        </div>
-      
-    </div>
+    <section className="pb-12 lg:pb-6 flex flex-col">
+        <h1 className="text-3xl pt-10 lg:pt-24 pb-4 leading-snug lg:pb-8 lg:text-5xl font-bold text-center tracking-tight text-[#26274b] relative">
+          Potřebujete půjčku, ale v bance vám jí nedají? <div className="cross-img-marketing"></div>
+        </h1>
+        <p className="text-xl lg:text-2xl text-center lg:w-2/3 mx-auto pt-3 lg:pt-6 pb-6 text-[#26274b] leading-normal">
+        My vám půjčku schválíme  <br className=" min-[450px]:hidden" />
+          <UnderlineText className="font-bold text-2xl pb-0.5 lg:pb-1">
+            do 72 hodin.
+          </UnderlineText>
+        </p>
+        <Button className="rounded-full text-[17px] font-bold w-fit mx-auto mb-6 mt-3 lg:mt-6 px-2.5 py-1.5 text bg-[#A2EB99]">
+          <a href="#kalkulacka">Chci si půjčit</a>
+        </Button>
+        <span className="text-xs opacity-60 text-center">
+          Stačí vyplnit krátký dotazník a my se s vámi spojíme.
+        </span>
+      </section>
   )
 }
 
